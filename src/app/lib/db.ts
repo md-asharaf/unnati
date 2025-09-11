4eb526e69e1dd21494d252ebbea83440856b1ca4
Md Asharaf <718romanempire@gmail.com>
2025-09-12 01:24:05 +0530

    Add database and token utility modules

---
+++ src/app/lib/db.ts
@@ -0,0 +1,43 @@
+import { PrismaClient } from "../../../generated/prisma";
+import RedisService from "../services/redis";
+import { logger } from "./logger";
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+    prisma = new PrismaClient();
+} else {
+    // @ts-ignore
+    if (!global.prisma) {
+        // @ts-ignore
+        global.prisma = new PrismaClient();
+    }
+    // @ts-ignore
+    prisma = global.prisma;
+}
+
+export const db = prisma;
+
+db.$connect()
+    .then(() => {
+        logger.info("[PRISMA] : connected to database");
+    })
+    .catch((error: any) => {
+        logger.error("[PRISMA] : failed to connect database : ", error);
+    });
+
+let redis: RedisService;
+
+if (process.env.NODE_ENV === "production") {
+    redis = new RedisService();
+} else {
+    // @ts-ignore
+    if (!global.redis) {
+        // @ts-ignore
+        global.redis = new RedisService();
+    }
+    // @ts-ignore
+    redis = global.redis;
+}
+
+export { redis };
---
+++ src/app/lib/token.ts
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+export type Payload = {
+    id: string;
+};
+const JWT_SECRET = process.env.JWT_SECRET;
+export const signToken = (payload: Payload, expiresIn: string = "7d") => {
+    return jwt.sign(payload, JWT_SECRET as string, {
+        expiresIn: expiresIn as any,
+    });
+};
+
+export const generateTokens = (payload: Payload) => {
+    const accessToken = signToken(payload, "1d");
+    const refreshToken = signToken(payload, "7d");
+    return {
+        accessToken,
+        refreshToken,
+    };
+};
+
+export const verifyToken = (token: string) => {
+    return jwt.verify(token, JWT_SECRET as string);
+};
