4ede3d01c1ef4d6e01060c17dce8d9979e6c429e
Md Asharaf <718romanempire@gmail.com>
2025-09-14 21:11:42 +0530

    Add name field to Admin model and schema

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,5 +1,11 @@
 import { z } from "zod";
 
+export const adminSchema = z.object({
+    id: z.uuid(),
+    email: z.email(),
+    name: z.string(),
+})
+
 export const loginSchema = z.object({
     email: z.email("invalid email format"),
 });
@@ -12,6 +18,9 @@ export const verifyLoginSchema = z.object({
         .max(6)
         .regex(/^\d+$/, "OTP must be a 6-digit number"),
 });
+
+export type Admin = z.infer<typeof adminSchema>;
+
 export type VerifyLogin = z.infer<typeof verifyLoginSchema>;
 
 export type Login = z.infer<typeof loginSchema>;
