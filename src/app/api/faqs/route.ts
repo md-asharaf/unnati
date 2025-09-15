7dca82bcdc318599b74b7a21a34d83ebbfa23708
Md Asharaf <718romanempire@gmail.com>
2025-09-15 20:14:20 +0530

    Add CRUD operations for FAQs with validation and pagination

---
+++ src/app/api/faqs/route.ts
@@ -0,0 +1,68 @@
+import { db } from "@/lib/db";
+import { CreateFAQ, createFaqSchema } from "@/schemas";
+import { NextRequest, NextResponse } from "next/server";
+import z from "zod";
+
+export const POST = async (req: NextRequest) => {
+    const { question, answer, topicId } = (await req.json()) as CreateFAQ;
+    let validatedData;
+    try {
+        validatedData = createFaqSchema.parse({ question, answer, topicId });
+    } catch (error) {
+        return NextResponse.json(
+            {
+                error: (error as z.ZodError).issues
+                    .map((issue) => issue.message)
+                    .join(", "),
+            },
+            { status: 400 },
+        );
+    }
+    const topic = await db.topic.findUnique({
+        where: { id: topicId },
+    });
+    if (!topic) {
+        return NextResponse.json({ error: "Topic not found" }, { status: 404 });
+    }
+    const faq = await db.faq.create({
+        data: validatedData,
+    });
+    return NextResponse.json(
+        {
+            data: {
+                faq,
+            },
+            message: "FAQ created successfully",
+        },
+        { status: 201 },
+    );
+};
+
+export const GET = async (req: NextRequest) => {
+    const topicId = req.nextUrl.searchParams.get("topicId");
+    const page = parseInt(req.nextUrl.searchParams.get("page") ?? "1", 10);
+    const limit = parseInt(req.nextUrl.searchParams.get("limit") ?? "10", 10);
+    const where = topicId
+        ? {
+              topicId,
+          }
+        : {};
+    const [faqs, total] = await Promise.all([
+        db.faq.findMany({
+            where,
+            skip: (page - 1) * limit,
+            take: limit,
+        }),
+        db.faq.count({ where }),
+    ]);
+    return NextResponse.json({
+        data: {
+            faqs,
+            total,
+            totalPages: Math.ceil(total / limit),
+            page,
+            limit,
+        },
+        message: "faqs fetched successfully",
+    });
+};
